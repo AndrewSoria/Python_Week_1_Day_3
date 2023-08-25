f2fe62fbe406f81c2837d7bc68175fc962746fcc
AndrewSoria <20378833-AndrewSoria@users.noreply.replit.com>
2023-08-25 14:35:19 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,79 @@
+#####################Bell Ringer#######################################
+# Find 3 objects around the room and create variables from it,
+# Insert those variables into an f-string sentence(look at slide 22)in repl.it
+
+
+# Familiarize yourself with the syntax of the print() function.
+# Print your name.
+# Print today's date.
+# Print the name of your favorite movie.
+
+# Print your name and age on separate lines using a single print() function.
+# Use f-strings to print a message like: "In 10 years, [Your Name] will be [Your Age + 10] years old."
+# Experiment with the sep and end parameters of the print() function to produce different outputs.
+
+###########################end bell ringer##################################
+name="Andrew Soria"
+age= 15
+objects="Pens, pencils, and Markers"
+date="August 25, 2023"
+movie="avengers:age of Ultron"
+print(f"My name is {name}  \nI am  {age}  Years old.")
+
+print(f"I think today is {date}") 
+print(f"Whats your favorite movie? Mine is  {movie}") 
+print(f"I cant wait to be  {age +6} In 6 years")
+print(f"Some of the most important items in a classroom are {objects}")
+
+
+
+
+
+
+###########################String Practice##################################
+# String Practice #1: try this in repl.it
+# Define a string containing your full name.
+# Print the first 3 letters of your name using string slicing.
+# Convert your name to uppercase and print the result.
+# Use string formatting to print "My name is [Your Name]."
+
+# String Practice #2
+# Write Python code that prints the following, by calling the print function just once:
+# Line 1
+# Line 2
+# Line 3
+
+# String Practice #3
+# Write Python code that prints the following:
+# A	B	C
+# D	E	F
+# G	H	I
+
+# String Practice #3
+# Write Python code that prints the following:
+# Backslash (\)
+# Forward Slash (/)
+
+##########################input practice#############################################
+# Input Practice #1
+# Write Python code that allows the user to enter their answer, by making them the following question:
+# What are you learning today?
+# Your code must be able to print to the screen whatever is entered by the user (use the print function).
+
+# Input Practice #2
+# Write Python code that allows the user to enter their answer, by making them the following question:
+# Where are you from?
+# Your code must be able to print to the screen whatever is entered by the user (use the print function).
+
+# Input Practice #3
+# Write Python code that displays the user's full name on the screen, by allowing them to enter their first and last name with the following instructions:
+# What is your name?
+# What is your surname?
+# The code must be able to print the user's first and last name on the screen, separated by a space.
+
+# Exercise:
+# Write a program that asks the user for their name and favorite color, then prints a message using both pieces of information.
+
+
+###########################time for a real challenge!!!!##################################
+# last slide
